3a3936b0dff9b7fcbc1308245fd851851fc3b5e3
HyunJae-Jo <dolxegod@gmail.com>
2020-03-11 13:42:48 +0900

    modify model
    add default value at 'is_complete' column

--- todo_board/models.py
+++ todo_board/models.py
@@ -16,7 +16,7 @@ class TbTodoList(models.Model):
     user_id = models.CharField(max_length=50, blank=True, null=True)
     title = models.CharField(max_length=200, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
-    is_complete = models.IntegerField(blank=True, null=True)
+    is_complete = models.IntegerField(blank=True, default=0)
     priority = models.IntegerField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
 
